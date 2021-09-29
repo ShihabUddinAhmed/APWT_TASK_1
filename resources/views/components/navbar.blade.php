eb68b46293efb36d8170a212f7d798e7355d405a
ShihabUddinAhmed <shihabahmed8957@gmail.com>
2021-09-29 13:28:20 +0600

    External Navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,7 @@
+<div>
+        <a href="{{ route('home') }}">Home</a> &emsp;
+        <a href="{{ route('service') }}">Services</a> &emsp;
+        <a href="{{ route('teams') }}">Our Teams</a> &emsp;
+        <a href="{{ route('aboutus') }}">About Us</a> &emsp;
+        <a href="{{ route('contact') }}">Contact Us</a>
+</div>
